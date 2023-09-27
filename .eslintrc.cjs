1f55d89282554dd9fbd300e9656977253e377a44
iamirinad <irka_dimcheva@abv.bg>
2023-09-27 16:12:17 +0300

    Implement delete a cabin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,9 +12,12 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'off',
     'react-refresh/only-export-components': [
-      'warn',
+      // 'warn',
+      'off',
       { allowConstantExport: true },
     ],
   },
-}
+};
