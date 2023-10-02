f213d7338d51a681758b25bc5df9a6a3e8a75a87
iamirinad <irka_dimcheva@abv.bg>
2023-10-03 01:38:48 +0300

    Duplicating cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -42,6 +42,8 @@ export async function createEditCabin(newCabin, id) {
   }
 
   // 2. Upload image
+  if (hasImagePath) return data;
+
   const { error: storageError } = await supabase
     .storage
     .from('cabin-images')
