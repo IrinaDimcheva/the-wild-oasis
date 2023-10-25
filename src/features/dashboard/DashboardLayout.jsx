32fd00571998bef676d9333187eeb8d830284070
iamirinad <irka_dimcheva@abv.bg>
2023-10-25 18:53:10 +0300

    Computing recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import Spinner from '../../ui/Spinner';
+import { useRecentBookings } from './useRecentBookings';
+import { useRecentStays } from './useRecentStays';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +11,14 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const { stays, isLoading: isLoading2 } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+
+  console.log(bookings);
+  console.log(stays);
+
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -54,6 +54,7 @@ export async function getBooking(id) {
 }
 
 // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
+// date: ISOString
 export async function getBookingsAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
@@ -69,11 +70,10 @@ export async function getBookingsAfterDate(date) {
   return data;
 }
 
-// Returns all STAYS that are were created after the given date
+// Returns all STAYS that were created after the given date
 export async function getStaysAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
-    // .select('*')
     .select("*, guests(fullName)")
     .gte("startDate", date)
     .lte("startDate", getToday());
